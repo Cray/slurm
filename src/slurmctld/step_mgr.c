4bfd409bef78b380a2b252a480a5cbff831dc786
Jason Sollom <jasons@cray.com>
2013-12-05 15:19:10 -0600

    Removing an unnecessary copyright.

--- src/slurmctld/step_mgr.c
+++ src/slurmctld/step_mgr.c
@@ -37,9 +37,6 @@
  *  with SLURM; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
 \*****************************************************************************/
-/*
- * Copyright 2013 Cray Inc. All Rights Reserved.
- */
 
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
